a41937fce6c0201b93c8e7c0f8d4ea7e0c60afcd
Mohamed Keita <mohkeita13@gmail.com>
2020-10-08 18:39:40 +0200

    serve valid data

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,9 +1,11 @@
 const User = require("../models/user");
+const redisClient = require("../config/redis").getClient();
 
 exports.follow = async (req, res, next) => {
     try {
         req.user.following.push(req.params.id);
         req.user.save();
+        redisClient.hdel("users", req.user.id);
         res.send({ message: "success" });
     } catch (err){
         next(err);
