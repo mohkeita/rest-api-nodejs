9690830442688274eb6ec66182e5b6d019c4e6b0
Mohamed Keita <mohkeita13@gmail.com>
2020-10-08 12:58:35 +0200

    integrate rate limiting

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const rateLimit = require("express-rate-limit");
 const mongoose = require('mongoose');
 
 const passportJWT = require('./middlewares/passportJWT')();
@@ -10,9 +11,19 @@ const errorHandler = require("./middlewares/errorHandler");
 const postRoutes = require('./routes/post');
 const authRoutes = require('./routes/auth');
 const followRoutes = require('./routes/follow');
+
 const app = express();
 
 app.use(cors());
+
+app.enable("trust proxy");
+
+const limiter = rateLimit({
+    windowMs: 10 * 1000,
+    max: 10
+});
+
+app.use(limiter);
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/rest-api-nodejs', {
